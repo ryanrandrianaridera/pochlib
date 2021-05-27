a0c20b482b9806d832bfd899d0bc84b91c24eb78
Ryan RANDRIANARIDERA <ryan.randria@gmail.com>
2021-05-27 10:34:22 +0200

    add functions + call API

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,158 @@
-const addBookBtn = document.createElement("div");
-let titleH2 = document.querySelector('.h2');
+// add function to insert new element with "myBooks" parent Div, and "hr" last target
 
-// Add btn1 Ajouter un livre
-addBookBtn.innerHTML = `<div class="addbook_div" id="addbook_id"><button onclick="addBook()" type="button" class="addbookbtn" id="btn1">Ajouter un livre</button></div>`;
-titleH2.after(addBookBtn);
-//console.log(addBookBtn);
-let addBookD = document.getElementById("addbook_id");
-
-addBookBtn.after(addBookD);
-
-// function addBook() {
-// //alert ("Hello");
-// //addBookBtn.style.display = "none";
-// addBookBtn.remove();
-// }
-function addBook(){
-    addBookD.innerHTML = `<form class="form" id="SearchForm"><div id="searcharea"><label for="titleNameField">Titre du livre<br/></label><input class="search" id="titleNameField" type="search" name="titleNameField"><br/><label for="AuthorNameField">Auteur<br/></label><input class="search" id="AuthorNameField"type="search" name="AuthorNameField" ><br/><button type="button"  class="btn searchButton" onclick="search()">Rechercher</button></br><button type="button"  onclick="window.location.href='index.html';" class="btn cancelButton" id="cancelButton" >Annuler</button></div></form>`;
-    
+function insertElement(elt) {
+  const parentDiv = document.getElementById("myBooks");
+  const target = document.getElementsByTagName("hr")[0];
+  parentDiv.insertBefore(elt, target);
 }
 
-addBookBtn.addEventListener("click" , addBook);
+// function to create field
+
+function createField(label, input) {
+  const field = document.createElement("div");
+  field.classList.add("form__field");
+  field.appendChild(label);
+  field.appendChild(input);
+  return field;
+}
+
+// function to create form
+
+function createForm() {
+  const form = document.createElement("form");
+  form.classList.add("form");
+
+  const labelTitle = document.createElement("label");
+  const labelTitleTxt = document.createTextNode("Titre du livre");
+  labelTitle.setAttribute("for", "intitle");
+  labelTitle.appendChild(labelTitleTxt);
+
+  const title = document.createElement("input");
+  title.setAttribute("type", "text");
+  title.setAttribute("name", "intitle");
+  title.setAttribute("id", "book-title");
+
+  const labelAuthor = document.createElement("label");
+  const labelAuthorTxt = document.createTextNode("Auteur");
+  labelAuthor.setAttribute("for", "inauthor");
+  labelAuthor.appendChild(labelAuthorTxt);
+
+  const author = document.createElement("input");
+  author.setAttribute("type", "text");
+  author.setAttribute("name", "inauthor");
+  author.setAttribute("id", "author");
+
+  const searchBtn = document.createElement("button");
+  searchBtn.innerHTML = "Rechercher";
+  searchBtn.classList.add("btn");
+  searchBtn.onclick = (event) => {
+    event.preventDefault();
+    searchBook();
+    title.value = "";
+    title.focus();
+    author.value = "";
+  };
+
+  const dropBtn = document.createElement("button");
+  dropBtn.innerHTML = "Annuler";
+  dropBtn.classList.add("btn", "btn--cancel");
+  dropBtn.onclick = (event) => {
+    event.preventDefault();
+    cancelSearch(form);
+  };
+
+  form.appendChild(createField(labelTitle, title));
+  form.appendChild(createField(labelAuthor, author));
+  form.appendChild(searchBtn);
+  form.appendChild(dropBtn);
+  insertElement(form);
+  form.style.display = "none";
+}
+
+// function to display form
+
+function displayForm() {
+  const btn = document.getElementById("addBook");
+  const form = document.getElementsByClassName("form")[0];
+  const title = document.getElementById("book-title");
+  if (btn !== null && form !== null) {
+    btn.style.display = "none";
+    form.style.display = "block";
+    title.focus();
+  }
+}
+
+// function to add button to add a book
+function addButton() {
+  const addBookBtn = document.createElement("button");
+  addBookBtn.innerHTML = "Ajouter un livre";
+  addBookBtn.classList.add("btn", "btn--center");
+  addBookBtn.id = "addBook";
+  insertElement(addBookBtn);
+  addBookBtn.onclick = displayForm;
+}
+
+// function to add result container
+function createResultsContainer() {
+  const resultsContainer = document.createElement("div");
+  resultsContainer.id = "res-output";
+  resultsContainer.classList.add("res-container");
+  resultsContainer.style.display = "none";
+  const resultsLine = document.createElement("hr");
+  const resultsTitle = document.createElement("h2");
+  resultsTitle.innerHTML = "Résultats de recherche";
+  const resultsGrid = document.createElement("div");
+  resultsGrid.classList.add("res-grid");
+  resultsGrid.id = "list-grid";
+  resultsContainer.appendChild(resultsLine);
+  resultsContainer.appendChild(resultsTitle);
+  resultsContainer.appendChild(resultsGrid);
+  insertElement(resultsContainer);
+}
+
+// function to search book with XMLHttpRequest with API Google Book
+function searchBook() {
+  let title = document.getElementById("book-title");
+  let author = document.getElementById("author");
+
+  request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+      let response = JSON.parse(this.responseText);
+
+      if (response.items.length > 0) {
+        response.items.forEach((book) => {
+          // creation d'un livre
+          createOneBook(book);
+        });
+      }
+    }
+  };
+  request.open(
+    "GET",
+    "https://www.googleapis.com/books/v1/volumes?q=" +
+      title.value +
+      "+inauthor:" +
+      author.value
+  );
+  request.send();
+}
+
+// function to create Book
+function createOneBook(book) {
+  console.log("createOneBook", book);
+}
+
+//function cancel search
+function cancelSearch(form) {
+  const btn = document.getElementById("addBook");
+  form.style.display = "none";
+  btn.style.display = "block";
+}
+
+function onload() {
+  createForm();
+  addButton();
+}
+
+window.addEventListener("load", onload);
